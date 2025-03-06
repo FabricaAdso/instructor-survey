7ac66f2157e84e6caf744964c990200c88b56552
AntonioRodriguez-2774315 <anttoniio2312@gmail.com>
2025-03-06 11:30:39 -0500

    se puede realizar el cargue masivo, faltan algunas validaciones y alertas

--- database/migrations/2024_11_24_135450_create_courses_table.php
+++ database/migrations/2024_11_24_135450_create_courses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('code');
+            $table->string('code')->unique();
 
             $table->unsignedBigInteger('program_id');
             $table->foreign('program_id')->references('id')->on('programs');
