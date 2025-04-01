e4a5ba53397790c96fa2c1699d42c37bbf225694
AntonioRodriguez-2774315 <anttoniio2312@gmail.com>
2025-04-01 16:51:47 -0500

    se soluciona erroe al iniciar sesion como lider de area

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -77,7 +77,7 @@ class AuthController extends Controller {
         }
 
         Auth::guard('areaLeader')->login($user);
-        return redirect()->route('admin.dashboard');
+        return redirect()->route('areaLeader');
     }
 
     public function login(Request $request)
